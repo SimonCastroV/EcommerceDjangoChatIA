fdce5587f775d2e2fc1b035e2a3487f41f818d53
SimonCastroV <142184921+SimonCastroV@users.noreply.github.com>
2025-10-18 18:51:40 -0500

    Final: evidencia chat apil

--- chat/views.py
+++ chat/views.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 from .models import ChatMessage
 from products.models import Product
 from .ai_service import GeminiService
-
+from django.views.decorators.csrf import csrf_exempt
 
 def chat_ui(request):
     """Muestra la interfaz del chat"""
@@ -19,11 +19,12 @@ def chat_ui(request):
     return render(request, "chat/chat.html", {"session_id": session_id})
 
 
+
+@csrf_exempt
 @require_POST
 def chat_message_api(request):
     """Recibe el mensaje del usuario y devuelve la respuesta de Gemini"""
     try:
-      
         data = json.loads(request.body)
         session_id = data.get("session_id")
         user_text = data.get("message", "").strip()
@@ -31,26 +32,19 @@ def chat_message_api(request):
         if not user_text:
             return JsonResponse({"error": "Mensaje vacío"}, status=400)
 
-      
         ChatMessage.objects.create(session_id=session_id, role="user", message=user_text)
-
         context = list(ChatMessage.objects.filter(session_id=session_id).order_by("created_at")[:6])
-
         products = Product.objects.all()
 
-        
         try:
             ai = GeminiService()
             ai_response = ai.generate_response(user_text, products, context)
         except Exception as e:
-            
             traceback.print_exc()
             ai_response = f"⚠️ Error al conectar con la IA: {e}"
 
-        # Guardar respuesta de la IA
         ChatMessage.objects.create(session_id=session_id, role="assistant", message=ai_response)
 
-       
         return JsonResponse({
             "session_id": session_id,
             "user_message": user_text,
